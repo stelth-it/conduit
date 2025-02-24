f1bf1b80178510eec9b1529bca60def9471f2ca6
Patrick <patrick@pop-os.localdomain>
2025-02-24 13:07:50 -0800

    defaults

---
+++ test/conduit_web/controllers/error_html_test.exs
@@ -0,0 +1,14 @@
+defmodule ConduitWeb.ErrorHTMLTest do
+  use ConduitWeb.ConnCase, async: true
+
+  # Bring render_to_string/4 for testing custom views
+  import Phoenix.Template
+
+  test "renders 404.html" do
+    assert render_to_string(ConduitWeb.ErrorHTML, "404", "html", []) == "Not Found"
+  end
+
+  test "renders 500.html" do
+    assert render_to_string(ConduitWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+  end
+end
