e836855901674ad57b3518742fdf24d8cd939263
patrick <patrick@pjslab.net>
2025-06-19 15:29:52 -0700

    fix: change our encrypted binary type so it can be used in embedded schemas.
    see this github issue for more info: https://github.com/danielberkompas/cloak/issues/84

--- lib/conduit/vault/encrypted_binary.ex
+++ lib/conduit/vault/encrypted_binary.ex
@@ -1,4 +1,18 @@
 defmodule Conduit.Vault.EncryptedBinary do
   @moduledoc false
   use Cloak.Ecto.Binary, vault: Conduit.Vault
+
+  def embed_as(:json), do: :dump
+
+  def dump(nil), do: {:ok, nil}
+
+  def dump(value) do
+    with {:ok, encrypted} <- super(value) do
+      {:ok, Base.encode64(encrypted)}
+    end
+  end
+
+  def load(nil), do: {:ok, nil}
+
+  def load(value), do: super(Base.decode64!(value))
 end
