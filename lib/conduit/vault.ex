a923f54364a3b5cf188abe4702ec41c0c664bc3a
patrick <patrick@pjslab.net>
2025-06-30 15:37:53 -0700

    chore: change secret fetching so that we rely on the environment.
    Relying on binaries to be present on the runtime environment, and that
    outbound internet connections will be allowed, makes deployment harder.

--- lib/conduit/vault.ex
+++ lib/conduit/vault.ex
@@ -16,63 +16,22 @@ defmodule Conduit.Vault do
       Keyword.put(config, :ciphers,
         default: {
           Cloak.Ciphers.AES.GCM,
-          tag: "Conduit.AES.V1", key: get_key(), iv_length: 12
+          tag: "Conduit.AES.V1", key: get_key_from_env("CLOAK_KEY"), iv_length: 12
         }
       )
 
     {:ok, config}
   end
 
-  @doc """
-  Retrieves the cloak key from onepassword CLI tool.
-  Will raise if the tool is not present or if anything
-  goes wrong with fetching or decoding the key.
-  """
-  def get_key do
-    with :ok <- check_for_exec(),
-         {:ok, path} <- get_op_path("CLOAK_KEY_OP_PATH"),
-         {:ok, encoded_key} <- run_op(path),
-         {:ok, bin_key} <- decode_key(encoded_key) do
-      bin_key
-    else
-      {:error, message} -> raise __MODULE__.Error, message
-    end
+  def get_key_from_env(key_var_name) do
+    value = System.fetch_env!(key_var_name)
+    decode_key!(value)
   end
 
-  defp decode_key(encoded_key) do
+  defp decode_key!(encoded_key) do
     case Base.decode64(encoded_key) do
-      {:ok, v} -> {:ok, v}
-      :error -> {:error, "key successfully fetched, but could not be decoded!"}
-    end
-  end
-
-  defp get_op_path(env_variable) do
-    case System.fetch_env(env_variable) do
-      {:ok, v} ->
-        {:ok, v}
-
-      :error ->
-        {:error, "environment variable with name #{env_variable} not found."}
-    end
-  end
-
-  defp run_op(vault_path) do
-    # we want any stderr messages to be included in our exception.
-    case System.cmd("op", ["read", vault_path], stderr_to_stdout: true) do
-      {key, 0} ->
-        # stdout adds a trailing newline
-        {:ok, String.trim(key)}
-
-      {message, _} ->
-        {:error, "onepassword could not retrieve key.  Error received:\n#{message}"}
-    end
-  end
-
-  defp check_for_exec do
-    if :os.find_executable(~c"op") do
-      :ok
-    else
-      {:error, "onepassword binary not found"}
+      {:ok, v} -> v
+      :error -> raise __MODULE__.Error, "key successfully fetched, but could not be decoded!"
     end
   end
 end
