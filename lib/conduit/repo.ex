a78ddff9e98b851575ca06cc7cc16a9094090c36
patrick <patrick@pjslab.net>
2025-07-09 23:01:37 -0700

    chore: convenience functions for schema
    needed to create what are referred to as 'partitions' in ecto.

--- lib/conduit/repo.ex
+++ lib/conduit/repo.ex
@@ -2,4 +2,24 @@ defmodule Conduit.Repo do
   use Ecto.Repo,
     otp_app: :conduit,
     adapter: Ecto.Adapters.Postgres
+
+  @spec schema_exists?(schema_name :: String.t()) :: boolean()
+  def schema_exists?(schema_name) do
+    {:ok, result} =
+      query("""
+      select schema_name
+      from information_schema.schemata;
+      """)
+
+    [schema_name] in result.rows
+  end
+
+  def create_schema_if_absent(schema_name) do
+    if schema_exists?(schema_name) do
+      :ok
+    else
+      {:ok, _} = query("create schema #{schema_name}")
+      :ok
+    end
+  end
 end
