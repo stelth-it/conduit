8f0e81f1b80841b0a381e9cf5b88e8328dd0fcd7
patrick <patrick@pjslab.net>
2025-07-14 20:20:27 -0700

    docs: docstring and typespecs added

--- lib/conduit/repo.ex
+++ lib/conduit/repo.ex
@@ -3,6 +3,10 @@ defmodule Conduit.Repo do
     otp_app: :conduit,
     adapter: Ecto.Adapters.Postgres
 
+  @doc """
+  Checks if a schema with `schema_name` already exists in the 
+  database.
+  """
   @spec schema_exists?(schema_name :: String.t()) :: boolean()
   def schema_exists?(schema_name) do
     {:ok, result} =
@@ -14,6 +18,11 @@ defmodule Conduit.Repo do
     [schema_name] in result.rows
   end
 
+  @doc """
+  will create the given `schema_name` in the 
+  database if it does not already exist.
+  """
+  @spec create_schema_if_absent(schema_name :: String.t()) :: :ok
   def create_schema_if_absent(schema_name) do
     if schema_exists?(schema_name) do
       :ok
